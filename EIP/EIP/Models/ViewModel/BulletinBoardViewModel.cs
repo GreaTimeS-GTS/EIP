a1c5f54ac8bda5045b1a9bd3833a8bae28a87402
Siaoyu <75236313+ococo246453@users.noreply.github.com>
2021-04-07 13:50:14 +0800

    大彙整

--- EIP/EIP/Models/ViewModel/BulletinBoardViewModel.cs
+++ EIP/EIP/Models/ViewModel/BulletinBoardViewModel.cs
@@ -11,18 +11,27 @@ namespace EIP.Models.ViewModel
     {
         [DisplayName("布告欄編號")]
         public int 佈告欄ID { get; set; }
+
         [DisplayName("員工編號")]
         public int EmployeeID { get; set; }
+
         [DisplayName("發佈人姓名")]
         [Required(ErrorMessage = "請輸入發佈人姓名")]
         public string 中文姓名 { get; set; }
+
         [DisplayName("佈告欄標題")]
         [Required(ErrorMessage = "請輸入佈告欄標題")]
         public string 佈告欄標題 { get; set; }
+
         [DisplayName("佈告欄內容")]
         public string 佈告欄內容 { get; set; }
+
         [DisplayName("發佈日期")]
         [Required(ErrorMessage = "請輸入發布日期")]
         public System.DateTime 發布日期 { get; set; }
+
+        [DisplayName("訊息重要度")]
+        [Required(ErrorMessage = "請輸入訊息重要度")]
+        public string 訊息重要度 { get; set; }
     }
 }
